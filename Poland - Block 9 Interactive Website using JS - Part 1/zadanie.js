970495c59bf6e06d227fde84bcf5b5f949e9b175
martynka30 <32480411+martynka30@users.noreply.github.com>
2018-01-03 18:24:16 +0100

    Add files via upload

---
+++ Poland - Block 9 Interactive Website using JS - Part 1/zadanie.js
@@ -0,0 +1,10 @@
+		function sprawdz()
+		{
+			var liczba = document.getElementById("pole").value;
+			
+			if (liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
+			else if (liczba<0) document.getElementById("wynik").innerHTML="ujemna";
+			else if (liczba==0) document.getElementById("wynik").innerHTML="zero";
+			else document.getElementById("wynik").innerHTML="to nie jest liczba. You know nothing John Snow";
+		
+		}
